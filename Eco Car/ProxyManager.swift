7e374bee558e45533ec92c484b9631da9d54f4e5
Sachin <sachinbhandari1505@gmail.com>
2020-06-27 13:40:20 +0530

    First Commit

---
+++ Eco Car/ProxyManager.swift
@@ -0,0 +1,199 @@
+//
+//  ProxyManager.swift
+//  Eco Car
+//
+//  Created by Sachin Bhandari on 23/06/20.
+//  Copyright © 2020 Sachin Bhandari. All rights reserved.
+//
+
+import Foundation
+import SmartDeviceLink
+
+
+class ProxyManager: NSObject {
+    
+//TODO:- Change appID, port and ipAddress here.
+    private let appName = "Eco Car"
+    private let appId = "1505"
+    private let appIpAddress = "m.sdl.tools"
+    private let appPort: UInt16 = 13625
+    
+
+    // Manager
+    fileprivate var sdlManager: SDLManager!
+
+    // Singleton
+    static let sharedManager = ProxyManager()
+
+    fileprivate var firstHMILevel: SDLHMILevel = .none
+
+
+
+    private override init() {
+        super.init()
+        firstHMILevel = .none
+        // Used for USB Connection
+//        let lifecycleConfiguration = SDLLifecycleConfiguration(appName: appName, fullAppId: appId)
+
+        // Used for TCP/IP Connection
+        let lifecycleConfiguration = SDLLifecycleConfiguration(
+                   appName: appName,
+                   fullAppId: appId,
+                   ipAddress: appIpAddress,
+                   port: appPort
+        )
+        
+        // App icon image
+        if let appImage = UIImage(named: "carLogo") {
+            let appIcon = SDLArtwork(image: appImage, name: "carLogo", persistent: true, as: .PNG)
+            lifecycleConfiguration.appIcon = appIcon
+        }
+
+        lifecycleConfiguration.shortAppName = appName
+
+        let configuration = SDLConfiguration(lifecycle: lifecycleConfiguration, lockScreen: .enabled(), logging: .default(), fileManager: .default(), encryption: nil)
+
+        sdlManager = SDLManager(configuration: configuration, delegate: self)
+        sdlManager.delegate = self
+
+        let lockScreenConfiguration = SDLLockScreenConfiguration.enabled()
+        lockScreenConfiguration.displayMode = .always
+
+        let isAllowed = sdlManager.permissionManager.isRPCAllowed("SubscribeVehicleData")
+
+        print("isAlowed: \(isAllowed)")
+
+
+        self.sdlManager.subscribe(to: .SDLDidReceiveVehicleData, observer: self, selector: #selector(self.vehicleDataAvailable(_:)))
+
+    }
+
+    func connect() {
+        // Start watching for a connection with a SDL Core
+        sdlManager.start { (success, error) in
+            if success {
+
+                print("SDL Connected Successfully")
+
+            }
+        }
+    }
+
+}
+
+//MARK:- SDLManagerDelegate
+extension ProxyManager: SDLManagerDelegate {
+  func managerDidDisconnect() {
+    print("Manager disconnected!")
+    
+    DispatchQueue.main.async {
+        self.sdlManager.stop()
+    }
+  }
+    
+
+  func hmiLevel(_ oldLevel: SDLHMILevel, didChangeToLevel newLevel: SDLHMILevel) {
+      if newLevel != .none && oldLevel == .none {
+          // This is our first time in a non-NONE state
+          firstHMILevel = newLevel
+
+        getVihicleData()
+      }
+
+
+      switch newLevel {
+      case .full: break                // The SDL app is in the foreground
+          // Always try to show the initial state to guard against some possible weird states. Duplicates will be ignored by Core.
+//          showInitialData()
+      case .limited: break        // An active NAV or MEDIA SDL app is in the background
+      case .background: break     // The SDL app is not in the foreground
+      case .none: break           // The SDL app is not yet running
+      default: break
+      }
+  }
+
+
+}
+
+
+// MARK:- Geta Data
+
+extension ProxyManager {
+
+    func unsubscribeToVehicleOdometer() {
+        let unsubscribeToVehicleOdometer = SDLUnsubscribeVehicleData()
+        unsubscribeToVehicleOdometer.odometer = true as NSNumber & SDLBool
+        sdlManager.send(request: unsubscribeToVehicleOdometer) { (request, response, error) in
+            guard let response = response, response.resultCode == .success else { return }
+//            self.resetOdometer()
+
+            print("Unsub : \(response)")
+        }
+    }
+    
+    
+    @objc func vehicleDataAvailable(_ notification: SDLRPCNotificationNotification) {
+        
+        if let response = notification.notification as? SDLOnVehicleData {
+            
+            if let fuel = response.instantFuelConsumption {
+                let info = ["fuel" : fuel.doubleValue,
+                            "Trial" : "Second"] as [String : Any]
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "uiInfoUpdate"), object: nil, userInfo: info)
+                print("onVehicleData : \(response)")
+            }
+            
+            if let odo = response.odometer {
+                let info = ["odometer" : odo.doubleValue]
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "uiInfoUpdate"), object: nil, userInfo: info)
+            }
+            
+            if let bodyInfo = response.bodyInformation {
+                let info = ["engineStatus" : bodyInfo.ignitionStatus.rawValue.rawValue]
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "uiInfoUpdate"), object: nil, userInfo: info)
+            }
+        }
+        
+    }
+    
+
+    func getVihicleData() {
+
+        let vehicleData = SDLGetVehicleData() // to get data on first time
+        vehicleData.odometer = NSNumber(true)
+        vehicleData.bodyInformation = NSNumber(true)
+        vehicleData.instantFuelConsumption = NSNumber(true)
+        vehicleData.vin = NSNumber(true)
+
+        let subscribeToVehicleData = SDLSubscribeVehicleData() // to get data in notification
+        subscribeToVehicleData.odometer = true as NSNumber & SDLBool
+        subscribeToVehicleData.instantFuelConsumption = true as NSNumber & SDLBool
+        subscribeToVehicleData.bodyInformation = true as NSNumber & SDLBool
+
+        let rpcRequest = [vehicleData, subscribeToVehicleData]
+
+        sdlManager.send(rpcRequest, progressHandler: { (request, response, err, value) in
+            
+            if let response = response as? SDLGetVehicleDataResponse {
+                                
+                guard let fuel = response.instantFuelConsumption, let odo = response.odometer, let bodyInfo = response.bodyInformation, let vin = response.vin else { return }
+                
+                let info = ["odometer" : odo.doubleValue,
+                            "fuel" : fuel.doubleValue,
+                            "engineStatus" : bodyInfo.ignitionStatus.rawValue.rawValue,
+                            "vin" : vin] as [String : Any]
+
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "uiInfoUpdate"), object: nil, userInfo: info)
+            }
+            
+        }, completionHandler: nil)
+        
+        
+        let vehicleInfo = SDLVehicleType()
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "uiInfoUpdate"), object: nil, userInfo: ["vehicleInfo" : vehicleInfo])
+
+    }
+    
+    
+}
+
--- Eco Car/ViewController.swift
+++ Eco Car/ViewController.swift
@@ -7,14 +7,134 @@
 //
 
 import UIKit
+import SmartDeviceLink
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var engineStatus: UILabel!
+    @IBOutlet weak var odometer: UILabel!
+    @IBOutlet weak var remainingFuel: UILabel!
+    @IBOutlet weak var vinLbl: UILabel!
+    @IBOutlet weak var finalResult: UILabel!
+
+    
+    var startO = Double()
+    var endO = Double()
+    
+    var startF = Double()
+    var endF = Double()
+    
+    var fuelValue = Double()
+    var odoValue = Double()
+    
+
+//    fileprivate var sdlManager: SDLManager!
+    
+    let pm = ProxyManager.self
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("Started")
+        
+//        self.odometer.text = "\(pm.sharedManager.odometerValue) mile(s)"
+        
+//        perform(#selector(showAlert), with: self, afterDelay: 3.0)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.uiInfoUpdate), name: NSNotification.Name(rawValue: "uiInfoUpdate"), object: nil)
+
+
     }
+    
+    
+    @objc func uiInfoUpdate(notification: NSNotification) {
 
+        if let info = notification.userInfo as? [String : Any] {
 
+            DispatchQueue.main.async {
+
+                if let odoVal = info["odometer"] as? Double {
+                    
+                    self.odometer.text = "\(odoVal) mile(s)"
+                    self.odoValue = odoVal
+                    
+                }
+                                
+                if let fuel = info["fuel"] as? Double {
+                    
+                    self.remainingFuel.text = "\(fuel) gallon(s)"
+                    self.fuelValue = fuel
+                    
+                }
+                
+                if let engineStatus = info["engineStatus"] as? String {
+                
+                    self.engineStatus.text = "\(engineStatus)"
+                    if self.engineStatus.text == "START" {
+                        self.startO = self.odoValue
+                        self.startF = self.fuelValue
+                    } else if self.engineStatus.text == "OFF" {
+                        self.endO = self.odoValue
+                        self.endF = self.fuelValue
+                        self.finalCalulations()
+                    }
+
+                }
+                
+                if let vin = info["vin"] as? String {
+                    print("Vin : \(vin)")
+                    self.vinLbl.text = "\(vin)"
+                    
+                }
+                
+                if let vehicleInfo = info["vehicleInfo"] as? SDLVehicleType {
+                    print("vehicleInfo: \(vehicleInfo)")
+//                    print("vehicleInfo: \(vehicleInfo.make!)") // Name of Manufacturer
+//                    print("vehicleInfo: \(vehicleInfo.model!)") // Model Name
+//                    print("vehicleInfo: \(vehicleInfo.modelYear!)") // Moder Year
+//                    print("vehicleInfo: \(vehicleInfo.trim!)") // trim of the vehicle
+                    
+                    // TODO:- Display Vehicle Information here
+                    
+                }
+
+                print("info: \(info)")
+            }
+        }
+        
+    }
+    
+    
+    func finalCalulations() {
+        
+        print("""
+            finalOdo : \(endO) - \(startO)
+            finalFuel : \(startF) - \(endF)
+            """)
+        let finalOdo = endO - startO
+        let finalFuel = startF - endF
+        print("finalOdo : \(finalOdo)\nfinalFuel : \(finalFuel)")
+        
+        let result = "Car has run : \(Float(finalOdo)) mile(s) in \(Float(finalFuel)) gallon(s)\nAverage is : \(finalOdo/finalFuel) mpg"
+        DispatchQueue.main.async {
+            
+            self.showAlert(msg: result)
+            
+        }
+    }
+    
+    
+    
+    @objc func showAlert(msg: String) {
+        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: .alert)
+        let dismiss = UIAlertAction(title: "Dismiss", style: .default) { (action) in
+            self.finalResult.isHidden = false
+            self.finalResult.text = msg
+
+        }
+        alert.addAction(dismiss)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
